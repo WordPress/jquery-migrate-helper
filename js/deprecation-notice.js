877691f186e517cbca8ad8a2d86a93690814cebe
Marius Jensen <marius@clorith.net>
2020-08-05 21:54:13 +0200

    Utilize the new jquery-migrate API fields to grab stack traces and surface the plugin or theme causing a notice

--- js/deprecation-notice.js
+++ js/deprecation-notice.js
@@ -1,9 +1,49 @@
 jQuery( document ).ready( function( $ ) {
+	function getPluginSlugFromTrace( trace ) {
+		let traceLines = trace.split( "\n" ),
+			regexSearch,
+			matches = null;
+
+		// Loop over each line in the stack trace
+		traceLines.forEach( function( line ) {
+			/*
+			 * The first few lines are going to be the jquery-migrate script, first instance
+			 * that is not one of them is probably a valid plugin or theme.
+			 *
+			 * If it's not the jquery-migrate script, identified by the version strings custom
+			 * addition by this plugin, do a regex match for a plugin or theme slug.
+			 *
+			 * If no regex match is made, it's likely a section of code triggered by something
+			 * else, so keep moving down the stack to find one.
+			 */
+			if ( -1 === line.indexOf( 'ejqmh' ) ) {
+				regexSearch = line.match( /\/(plugins|themes)\/(.+?)\//gm );
+
+				if ( regexSearch ) {
+					matches = regexSearch[0];
+					return true;
+				}
+			}
+		} );
+
+		// If the stack trace did not contain a matching plugin or theme, just return a null value.
+		return matches;
+	}
+
 	if ( jQuery.migrateWarnings.length >= 1 ) {
 		$( '#jquery-migrate-deprecation-notice' ).show();
 
 		jQuery.migrateWarnings.forEach( function( entry ) {
-			$( '#jquery-migrate-deprecation-list' ).append( '<li>' + entry + '</li>' );
+			let entryCause = getPluginSlugFromTrace( entry.trace );
+			let entryMessage = '<li>';
+
+			if ( entryCause ) {
+				entryMessage = entryMessage + entryCause + ': ';
+			}
+
+			entryMessage = entryMessage + entry.warning + '</li>';
+
+			$( '#jquery-migrate-deprecation-list' ).append( entryMessage );
 		} );
 	}
 } );
