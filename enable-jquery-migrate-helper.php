ae6eb3855feb3a76246c619f9525958d373de256
Marius Jensen <marius@clorith.net>
2020-11-18 01:57:27 +0100

    Allow the plugin to initiate on WordPress 5.6 or later

--- enable-jquery-migrate-helper.php
+++ enable-jquery-migrate-helper.php
@@ -3,7 +3,7 @@
 Plugin Name: Enable jQuery Migrate Helper
 Plugin URI: https://wordpress.org/plugins/enable-jquery-migrate-helper
 Description: Enable support for old and outdated plugins and themes during a jQuery update transitional phase.
-Version: 1.1.0
+Version: 1.2.0-alpha
 Requires at least: 5.4
 Tested up to: 5.5
 Requires PHP: 5.6
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Invalid request.' );
 }
 
-if ( version_compare( $GLOBALS['wp_version'], '5.6-alpha', '<' ) && ! class_exists( 'jQuery_Migrate_Helper' ) ) {
+if ( ! class_exists( 'jQuery_Migrate_Helper' ) ) {
 	include_once __DIR__ . '/class-jquery-migrate-helper.php';
 	add_action( 'plugins_loaded', array( 'jQuery_Migrate_Helper', 'init_actions' ) );
 }
