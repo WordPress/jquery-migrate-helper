e5ea2e6ed5af4694943443324380e165a497f8d5
Marius Jensen <marius@clorith.net>
2020-11-22 02:04:22 +0100

    Remove scheduled events on plugin removal

--- uninstall.php
+++ uninstall.php
@@ -15,3 +15,7 @@ delete_option( '_jquery_migrate_downgrade_version' );
 delete_option( '_jquery_migrate_deprecations_dismissed_notice' );
 delete_option( '_jquery_migrate_previous_deprecations_dismissed_notice' );
 delete_option( '_jquery_migrate_has_auto_downgraded' );
+
+if ( wp_next_scheduled( 'enable_jquery_migrate_helper_notification' ) ) {
+	wp_clear_scheduled_hook( 'enable_jquery_migrate_helper_notification' );
+}
