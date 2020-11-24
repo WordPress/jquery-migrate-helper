4d5f9963b14d7e3f49a825150b78862d2d0a255d
Tor-Björn Fjellner <tobifjellner@users.noreply.github.com>
2020-11-24 12:28:22 +0100

    Language review 4 (no-longer-needed)

--- templates/admin-notices/no-longer-needed.php
+++ templates/admin-notices/no-longer-needed.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Admin notice template encouraging removal of the plugin when it may no longer be needed.
+ * Admin notice template encouraging removal of this plugin when it may no longer be needed.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -16,18 +16,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	</p>
 
 	<p>
-		<?php _e( 'Please keep in mind that only notices on the public facing part of your site, or if you have disabled the display on the back end, will be logged and accounted for.', 'enable-jquery-migrate-helper' ); ?>
+		<?php _e( 'Please keep in mind that the log and the reports include errors encountered on the site admin pages (back end) only if you have disabled immediate display of errors in the back end.', 'enable-jquery-migrate-helper' ); ?>
 	</p>
 
 	<p>
-		<?php _e( 'This means you should still check that things work as expected after the plugin is disabled, and if you know there have been warnings in the admin pages, you may still need to reach out to the plugin or theme authors affected.', 'enable-jquery-migrate-helper' ); ?>
+		<?php _e( 'This means you should still check that things work as expected after you have disabled the plugin, and if you know there have been warnings in the admin pages, you may still need to reach out to the authors of the affected plugin or theme.', 'enable-jquery-migrate-helper' ); ?>
 	</p>
 
 	<?php if ( ! is_wp_version_compatible( '5.5.1' ) ) : ?>
 
 		<p>
 			<strong>
-				<?php _e( 'You are using a WordPress version prior to 5.5.1, this plugin also helps with a bug found in WordPress 5.5.0, you should update to version 5.5.1, or later, before deactivating the plugin.', 'enable-jquery-migrate-helper' ); ?>
+				<?php _e( 'You are using a WordPress version prior to 5.5.1. This plugin also helps with a bug found in WordPress 5.5.0. Please update WordPress to version 5.5.1, or later, before deactivating this plugin.', 'enable-jquery-migrate-helper' ); ?>
 			</strong>
 		</p>
 
