e38b812a26bd23bfa6cc3a101ca259dfed924092
Marius Jensen <marius@clorith.net>
2020-11-30 23:07:54 +0100

    Improve copy of the welcome/persistent notice box

--- templates/admin-notices/welcome.php
+++ templates/admin-notices/welcome.php
@@ -11,19 +11,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="notice <?php echo ( 'no' !== get_option( '_jquery_migrate_downgrade_version', 'no' ) ? 'notice-error' : 'notice-warning is-dismissible' ); ?> jquery-migrate-dashboard-notice" data-notice-id="jquery-migrate-notice">
 	<h2><?php _ex( 'jQuery Migrate Helper', 'Admin notice header', 'enable-jquery-migrate-helper' ); ?></h2>
 	<p>
-		<?php _e( 'Right now you are using the Enable jQuery Migrate Helper plugin to enable support for old JavaScript code that uses deprecated functions in the jQuery JavaScript library.', 'enable-jquery-migrate-helper' ); ?>
+		<?php _e( 'Right now, you are using a plugin to support some old code in this website’s jQuery JavaScript library. That code is deprecated, meaning it is basically obsolete and is losing the support of your themes and plugins and WordPress itself.', 'enable-jquery-migrate-helper' ); ?>
 		<br>
 		<strong>
-			<?php _e( 'Please note that this is a temporary solution which will only work between WordPress 5.5.0 and 5.6.0. This is NOT meant as a permanent fix for unsupported code.', 'enable-jquery-migrate-helper' ); ?>
+			<?php _e( 'This plugin, called Enable jQuery Migrate Helper, is not a fix. It is a temporary solution to give your plugin and theme authors time to update.', 'enable-jquery-migrate-helper' ); ?>
 		</strong>
 	</p>
 
 	<p>
-		<?php _e( 'If you get warnings, you should check for an updated version of the plugin or theme that generated the warning. There will very likely be one you can install. When you have updated your plugins and themes, and there are no more warnings, please deactivate Enable jQuery Migrate Helper.', 'enable-jquery-migrate-helper' ); ?>
-	</p>
-
-	<p>
-		<?php _e( '* A script, a file, or some other piece of code gets deprecated when its developers are in the process of replacing it with more modern code or removing it entirely.', 'enable-jquery-migrate-helper' ); ?>
+		<?php _e( 'If you get warnings, please check for updated versions of the plugin or theme that generated the warning. There will very likely be one you can install. When you have updated your plugins and themes, and there are no more warnings, please deactivate Enable jQuery Migrate Helper.', 'enable-jquery-migrate-helper' ); ?>
 	</p>
 
 	<?php if ( 'no' !== get_option( '_jquery_migrate_downgrade_version', 'no' ) ) : ?>
