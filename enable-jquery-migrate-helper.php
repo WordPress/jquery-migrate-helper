5fd8a2c4b383cec2e47c78d28728bfffb1cb3eec
Marius Jensen <marius@clorith.net>
2020-12-10 04:18:27 +0100

    Set new working versions

--- enable-jquery-migrate-helper.php
+++ enable-jquery-migrate-helper.php
@@ -3,7 +3,7 @@
 Plugin Name: Enable jQuery Migrate Helper
 Plugin URI: https://wordpress.org/plugins/enable-jquery-migrate-helper
 Description: Enable support for old and outdated plugins and themes during a jQuery update transitional phase.
-Version: 1.2.0-alpha
+Version: 1.3.0-alpha
 Requires at least: 5.4
 Tested up to: 5.5
 Requires PHP: 5.6
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: wordpressdotorg, clorith, azaozz
 Tags: jquery, javascript, update
 Requires at least: 5.4
-Tested up to: 5.5
-Stable tag: 1.1.0
+Tested up to: 5.6
+Stable tag: 1.2.0
 Requires PHP: 5.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
